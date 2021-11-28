39361227de14109e83c5f48c7f1a8bf78801dec8
jay <white.jay.jesse@gmail.com>
2021-11-28 15:27:55 +0200

    properly divided

--- day4/main.js
+++ day4/main.js
@@ -3,15 +3,11 @@ const getData = require("./script.js");
 
 let data = getData("\n\n");
 
+let reg = /\n/ig;
 
+for (let x = 0; x< data.length; x++) {
+    data[x] = data[x].replace(reg, " ").split(" ")
+}
+console.log(data)
 
-// data = data.filter(x => {
-//     if (x.length > 0){
-//         return x
-//     }
-// });
-console.log(typeof(data[1]))
-// for (let x = 0; x < data.length; x++) {
-//     console.log(data[x].length)
-// }
 
